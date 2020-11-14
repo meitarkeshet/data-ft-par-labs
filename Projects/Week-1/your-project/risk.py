f88fed3a8f7027a68a458c2a8a651e25461ddcd7
Meitar Keshet <meitarkeshet@gmail.com>
2020-11-14 17:37:03 +0100

    moved all into functions

--- Projects/Week-1/your-project/risk.py
+++ Projects/Week-1/your-project/risk.py
@@ -10,55 +10,70 @@
 #from colorama import Fore
 
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, soldiers):
         self.name = name
+        self.soldiers = soldiers
+        
+def play_game():
+    phase_initialize()
 
-#get list of players (limit: six players)
-players_lst = ["delete me"]
-while players_lst[-1] != "q":
-    players_lst.append(input("Please insert a player's name or ‘q’ once finished\n"))
-    if len(players_lst) > 7:
-        print("""This game is limited to 6 participents.\n
-              Let's start over.""")
-        players_lst = ["delete me"]
-players_lst = players_lst[1:-1]
+def phase_initialize():
+
+    #get list of players (limit: six players)
+    players_lst = ["delete me"]
+    while players_lst[-1] != "q":
+        players_lst.append(input("Please insert a player's name or ‘q’ once finished\n"))
+        if len(players_lst) > 7:
+            print("""This game is limited to 6 participents.\n
+                Let's start over.""")
+            players_lst = ["delete me"]
+    players_lst = players_lst[1:-1]
 
 
-print(players_lst)
+    #print(players_lst)
 
-#create object names
-players_enu = [""]
-for i, x in enumerate(range(len(players_lst))):
-    players_enu.append("player_"+str(i))
-players_enu = players_enu[1:]
-print(players_enu)
+    #create object names
+    players_enu = [""]
+    for i, x in enumerate(range(len(players_lst))):
+        players_enu.append("player_"+str(i))
+    players_enu = players_enu[1:]
+    #print(players_enu)
 
-#fix a max 
 
-# create objects from list of object names
-for i, x in enumerate(players_enu):
-    print(x)
-    globals()[x] = Player(players_lst[i])
-    #print(globals()[x].name)
+    # create objects from list of object names
+    for i, x in enumerate(players_enu):
+       # print(x)
+        globals()[x] = Player(players_lst[i], init_soldiers(len(players_lst)))
+       # print(globals()[x].name)
 
-print(player_1.name)
+    #print(player_1.name)
+    #print(player_1.soldiers)
+
 
 
 # --------------------------------------
 # BOARD SETUP
 
-"""
-If 2 are playing, see special instructions.If 3 are playing, each player counts out 35 Infantry.If 4 are playing, 
-each player counts out 30 Infantry.
-If 5 are playing, each player counts out 25 Infantry.If 6 are playing, each player counts out 20 Infantry.
-"""
-if len(players_lst) == 2:
-    print("2")
-elif len(players_lst) == 3:
-    print("3")
-elif len(players_lst) == 4:
-    print("4")
-elif len(players_lst) == 5:
-    print("5")
-elif len(players_lst) == 6:
-    print("6")
+def init_soldiers(num_players):
+    """
+    If 2 are playing, see special instructions.If 3 are playing, each player counts out 35 Infantry.If 4 are playing, 
+    each player counts out 30 Infantry.
+    If 5 are playing, each player counts out 25 Infantry.If 6 are playing, each player counts out 20 Infantry.
+    """
+    if num_players == 2:
+        return(40) # notice - it's not following the O.G rules. 
+    elif num_players == 3:
+        return(35)
+    elif num_players == 4:
+        return(30)
+    elif num_players == 5:
+        return(25)
+    elif num_players == 6:
+        print(20)
+        
+# --------------------------------------
+# RUN GAME
+
+play_game()
+
+print(player_1.soldiers)
