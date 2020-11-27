89e0fcdb46bc15dac77a89260370af0df63507fd
Meitar Keshet <meitarkeshet@gmail.com>
2020-11-27 16:11:13 +0100

    changing to mysql workbench

--- Labs/module_1/Pipelines-Project [Optional]/app.py
+++ Labs/module_1/Pipelines-Project [Optional]/app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_mysqldb import MySQL
+ 
 app = Flask(__name__)
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Mischief_1992'
+app.config['MYSQL_DB'] = 'flask'
+ 
+mysql = MySQL(app)
+
+db = MySQL(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    data_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debog=True)
+    app.run(debug=True)
